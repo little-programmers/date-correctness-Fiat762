1edf8736137b3077905eceed0d82dc7485eef15d
Fiat762 <43491919+Fiat762@users.noreply.github.com>
2018-10-12 22:45:48 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -5,7 +5,49 @@
 // Функция возращает true, если дата корректна, иначе - false.
 // Вы можете объявлять дополнительные функции, главное, чтобы присутствовала эта функция.
 // Вместо точки с запятой, открывайте фигурные скобки и начинайте писать.
-bool date_correctness(int day, int month, int year);
+bool date_correctness(int day, int month, int year) {
+	if ((year > 0) && (day > 0) && (month > 0)) {
+		switch (month) {
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			if (day > 0 && day <= 31) { return true; }
+			else
+			{
+				return false;
+			}
+			break;
+
+		case 2: if (year % 4 == 0 && year % 100 != 0 || (year%400==0)) {
+			if (day > 0 && day <= 29) { return true; }
+			else
+			{
+				return false;
+			}
+			break;
+		} else {
+			if (day > 0 && day <= 28) { return true; }
+			else
+			{
+				return false;
+			}
+		}
+		case 4: case 6: case 9: case 11:
+			if (day > 0 && day <= 30) { return true; }
+			else
+			{
+				return false;
+			}
+			break;
+			{
+		default: return false;
+			break;
+			}
+		}
+	}
+	else { return false; }
+	
+
+	
+}
 
 
 void test_date_correctness() {
@@ -27,5 +69,6 @@ void test_date_correctness() {
 
 int main() {
     test_date_correctness();
+	getchar();
     return 0;
 }
